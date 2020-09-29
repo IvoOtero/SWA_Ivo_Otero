c33ebbf7a14f240df74b465bb1b3f28e59cd4687
IvoOtero24 <ivo.tomas4@hotmail.com>
2020-09-29 12:59:57 +0200

    Coding Dojo 1: Creating a Stack without Collections and Generics in C#

---
+++ CodingDojo1/CodingDojo1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CodingDojo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //new Stack
+            Stack<Object> _stack1 = new Stack<Object>();
+
+
+            //new Items to push onto the stack
+            Item<Object> _item1 = new Item<Object>() { data = "Ivo", next = null };
+            Item<Object> _item2 = new Item<Object>() { data = 24, next = null };
+            Item<Object> _item3 = new Item<Object>() { data = new int[5], next = null };
+            Item<Object> _item4 = new Item<Object>() { data = "Miki", next = null };
+            Item<Object> _item5 = new Item<Object>() { data = "Malen", next = null };
+
+
+
+            //push new item on top of stack
+            _stack1.Push(_item1);
+            _stack1.Push(_item2);
+            _stack1.Push(_item3);
+            _stack1.Push(_item4);
+            _stack1.Push(_item5);
+
+
+            //print
+            Console.WriteLine(_item5.data);
+            printStack(_stack1);
+
+            //peek
+            Console.WriteLine(_stack1.Peek());
+
+            //pop
+            Console.WriteLine(_stack1.Pop());
+            printStack(_stack1);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private static void printStack(Stack<Object> stack)
+        {
+            if (stack._currentItem != null)
+            {
+                Console.WriteLine("The Stack will be printed in order: \n");
+                while (stack._currentItem.next != null)
+                {
+                    Console.WriteLine($" -> {stack._currentItem.data}");
+                    stack._currentItem = stack._currentItem.next;
+                }
+                Console.WriteLine($" -> {stack._currentItem.data}\n");
+            }
+            
+        }
+    }
+}
---
+++ CodingDojo1/CodingDojo1/Stack.cs
@@ -0,0 +1,64 @@
+using System;
+
+
+namespace CodingDojo1
+{
+    class Item<T>
+    {
+        public Item<T> next { get; set; } //saves the reference to the next element in the stack-list
+        public T data { get; set; }    // stores the data/value of the item (can be of any type)
+
+    }
+
+    class Stack<T>
+    {
+        public Item<T> _currentItem { get; set;  }   //first and current item on top of the stack
+
+
+        //returns object from top of the stack 
+        public T Peek()
+        {
+            if (_currentItem == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                return _currentItem.data;
+            }
+            
+
+        }
+
+        // The Push Method adds a new object to the top of the stack
+        public void Push(T item)
+        {
+            if (_currentItem == null)
+            {    
+                _currentItem = new Item<T>() {next = null, data = item}; //if the stack is empty, the new entry is placed on top of the stack
+            }
+            else
+            {
+                Item<T> tmp = new Item<T>() {next = _currentItem, data = item}; //new entry on top of the stack, with the current element being the next in the list
+                _currentItem = tmp; // new entry becomes the current item (now on top of the stack)
+            }
+        }
+
+
+        // the Pop Method removes the item on top of the stack (following the LIFO-Principle)
+        public T Pop()
+        {
+            if (_currentItem == null)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                T tmp = _currentItem.data; //data of the current item atop of stack is stored in a temporary element
+                _currentItem = _currentItem.next; //the second element of the list is placed atop of the stack (as cur element is removed)
+                return tmp; //the removed element stored in "tmp" is returned
+            }
+        }
+
+    }
+}
